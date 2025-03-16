49da41ad445d26c079ce52f8930f2da75fcf6dec
Thiago Gontijo <tjgontijo@gmail.com>
2025-03-16 12:59:40 -0300

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,93 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex flex-col min-h-screen">
+      {/* Header */}
+      <header className="bg-[#6bbbed] py-2 flex justify-center items-center shadow-md">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/images/system/logo_transparent.png"
+          alt="Logo Prof Didática"
+          width={100}
+          height={40}
+          className="py-1"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+      {/* Main Content */}
+      <main className="bg-[#fffaf3] flex-1 flex flex-col items-center justify-center py-8 px-4">
+        <div className="w-full max-w-md flex flex-col gap-6">
+          <Link 
+            href="https://profdidatica.com.br/operacoes-matematicas-em-pixel-art-divertidamente/p"
+            className="w-full transform transition-transform duration-300 hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            <div className="overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+              <Image
+                src="/images/banner/banner_pixel.jpg"
+                alt="Banner Pixel"
+                width={480}
+                height={150}
+                className="w-full h-auto"
+              />
+            </div>
+          </Link>
+          
+          <Link 
+            href="/projeto-literario"
+            className="w-full transform transition-transform duration-300 hover:scale-105"            
+          >
+            <div className="overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+              <Image
+                src="/images/banner/banner_projeto.jpg"
+                alt="Banner Projeto"
+                width={480}
+                height={150}
+                className="w-full h-auto"
+              />
+            </div>
+          </Link>
+          
+          <Link 
+            href="https://api.whatsapp.com/send?phone=551148635262&text=Ol%C3%A1,%20vim%20do%20instagram%20"
+            className="w-full transform transition-transform duration-300 hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
-          </a>
+            <div className="overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+              <Image
+                src="/images/banner/banner_suporte.jpg"
+                alt="Banner Suporte"
+                width={480}
+                height={150}
+                className="w-full h-auto"
+              />
+            </div>
+          </Link>
+          
+          <Link 
+            href="https://profdidatica.com.br/?utm_source=instagram&utm_medium=link_bio&utm_campaign=nossa_loja"
+            className="w-full transform transition-transform duration-300 hover:scale-105"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+              <Image
+                src="/images/banner/banner_loja.jpg"
+                alt="Banner Loja"
+                width={480}
+                height={150}
+                className="w-full h-auto"
+              />
+            </div>
+          </Link>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      
+      <footer className="bg-[#6bbbed] py-3 text-center text-sm text-gray-100">
+        &copy; {new Date().getFullYear()} Prof Didática - Todos os direitos reservados
       </footer>
     </div>
   );
