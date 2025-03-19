a73255e6b4108a9a4b26581d1b2910df9d7550ce
Thiago Gontijo <tjgontijo@gmail.com>
2025-03-19 20:26:46 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <UtmifyScripts />
-        <Claritycript />        
+        {/* <Claritycript />         */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
