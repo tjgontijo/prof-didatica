d96695ce4d0e7374f8a5512c51e0bfc1490278c4
Thiago Gontijo <tjgontijo@gmail.com>
2025-03-28 13:39:00 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,15 @@ module.exports = {
         swipe: {
           '0%, 100%': { transform: 'translateX(0)' },
           '50%': { transform: 'translateX(10px)' },
+        },
+        gradientX: {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' }
         }
       },
       animation: {
         'swipe': 'swipe 1.5s ease-in-out infinite',
+        'gradient-x': 'gradientX 5s ease infinite'
       }
     },
   },
