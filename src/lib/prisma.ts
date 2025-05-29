a674f2b9afbe77c2438726f51f4dbf9dcd449b7f
tjgontijo <tjgontijo@gmail.com>
2025-05-29 17:50:27 -0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = new PrismaClient({
   log:
-    process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['warn', 'error'],
+    process.env.NODE_ENV === 'development' ? ['info', 'warn', 'error'] : ['warn', 'error'],
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
