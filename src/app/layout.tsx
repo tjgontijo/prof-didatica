6096575d49cc36fceb04dde1cad7cd0b273fe417
Thiago Gontijo <tjgontijo@gmail.com>
2025-06-15 17:19:05 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { UtmifyScripts } from '@/scripts/utmifyScripts';
-import { ClarityScript } from '@/scripts/clarityScripts';
+// import { UtmifyScripts } from '@/scripts/utmifyScripts';
+// import { ClarityScript } from '@/scripts/clarityScripts';
 
 const inter = Inter({
   subsets: ['latin'],
