a41e3f22189d024a89d8600531db588fa3907283
Thiago Gontijo <tjgontijo@gmail.com>
2025-06-21 19:57:19 -0300

    Update seed-webhooks.ts

--- prisma/seeds/seed-webhooks.ts
+++ prisma/seeds/seed-webhooks.ts
@@ -1,8 +1,14 @@
 export const initialWebhooks = [
   {
     url: 'https://webhook.elev8.com.br/webhook/e6488ffd-004e-4e94-ae87-d4fc1c0b35b5',
-    events: ['order.created', 'order.paid'],
-    description: 'Webhook para o n8n - Eventos de pedido',
+    events: ['order.created'],
+    description: 'Webhook para o n8n - Evento de pedido criado',
+    active: true
+  },
+  {
+    url: 'https://webhook.elev8.com.br/webhook/e6488ffd-004e-4e94-ae87-d4fc1c0b35b5',
+    events: ['order.paid'],
+    description: 'Webhook para o n8n - Evento de pedido pago',
     active: true
   }
 ];
