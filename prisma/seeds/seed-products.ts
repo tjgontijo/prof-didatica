8b37e85578fe44247cd3128383ded26494942a19
Thiago Gontijo <tjgontijo@gmail.com>
2025-06-21 16:34:01 -0300

    Update seed-products.ts

--- prisma/seeds/seed-products.ts
+++ prisma/seeds/seed-products.ts
@@ -1,15 +1,15 @@
 export const initialProducts = [
   {
     name: 'Desafio Literário',
-    description: 'Fichas prontas, leitura gamificada, leitômetro, controle de leitura e mais. Um kit completo pra engajar sua turma de forma prática e divertida.',
+    description: '20 Fichas prontas, leitura gamificada com leiturômetro e controle de leitura. Um kit completo pra engajar sua turma de forma prática e lúdica.',
     imageUrl: '/images/products/desafio_literario_cover.webp',
     googleDriveUrl: 'https://drive.google.com/drive/folders/1dKXQPGdYNhDTl-W4_yS8kJ-wHRdN3y9E?usp=drive_link',
     slug: 'desafio-literario',
     price: 1200
   },
   {
-    name: '40 textos para o recurso Desafio Literário',
-    description: 'Coletânea com 40 textos feitos sob medida para usar com as fichas do Desafio Literário e enriquecer ainda mais as atividades.',
+    name: '40 textos para: Desafio Literário',
+    description: 'Coletânea com 40 textos feitos sob medida para usar com as fichas do Desafio Literário e enriquecer ainda mais as suas aulas.',
     imageUrl: '/images/products/textos_missao_literaria_cover.webp',
     googleDriveUrl: 'https://drive.google.com/drive/folders/164nlnziZpknRlr0Rvi5XUTI39n6aFZ_X?usp=drive_link',
     slug: 'textos-para-desafio-literario',
