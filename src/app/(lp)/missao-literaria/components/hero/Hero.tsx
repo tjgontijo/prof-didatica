cf8353a02f7c315ab2823230314dd267a364fd14
Thiago Gontijo <tjgontijo@gmail.com>
2025-07-08 19:50:28 -0300

    Update Hero.tsx

--- src/app/(lp)/missao-literaria/components/hero/Hero.tsx
+++ src/app/(lp)/missao-literaria/components/hero/Hero.tsx
@@ -25,14 +25,13 @@ export default function Hero() {
           <span className="bg-yellow-100 px-1 py-0.5 rounded text-[#1D3557] font-extrabold">
             3X MAIS LIVROS E TEXTOS
           </span>{' '}
-          EM MENOS DE{' '}
-          <span className="underline decoration-[#a8dadc] decoration-4">5 AULAS</span>
+          JÁ NAS PRIMEIRAS{' '}
+          <span className="underline decoration-[#a8dadc] decoration-4">AULAS</span>
         </span>
       </h1>
       <h2 className="text-lg md:text-xl text-[#1D3557] mb-6 max-w-3xl mx-auto font-normal leading-relaxed">
-        Você recebe imediatamente no WhatsApp 20 fichas literárias, dois modelos de leiturômetro pra
-        transformar a leitura em uma competição saudável em sala, dois modelos de tabela pra acompanhar tudo e um bônus especial que vou revelar no final dessa página. 
-        <br />Prático e direto: imprima e use com seus alunos já na próxima aula.
+        Você recebe imediatamente no WhatsApp <strong>20 fichas literárias</strong>, <strong> 2 modelos de leiturômetro</strong>, <strong> 2 modelos de tabela</strong> para acompanhar o progresso dos alunos e um <strong>bônus especial</strong> que vou revelar no final dessa página. 
+        <br /><div className="my-3 py-2 px-4 bg-yellow-100 rounded-lg inline-block font-semibold text-[#1D3557] border-l-4 border-[#457B9D] shadow-sm">Prático e direto: imprima e use com seus alunos já na próxima aula!</div>
         <br />Veja abaixo uma amostra do material:
       </h2>      
       <div className="w-full max-w-2xl mx-auto mb-0">
