2facd8db461f5047d3d7026397ef0f9f29372480
Thiago Gontijo <tjgontijo@gmail.com>
2025-07-19 17:00:12 -0300

    Remove Test A/B

--- src/app/(lp)/desafio-literario/b/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-[#2c4f71] py-4 text-center text-white">
-      <div className="container mx-auto px-4">
-        <p>&copy; {new Date().getFullYear()} Prof Didática - Todos os direitos reservados</p>
-      </div>
-    </footer>
-  );
-}
