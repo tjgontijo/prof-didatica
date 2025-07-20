b712835978497d4c20fc2c1a3bfb56c6f3afeb3b
Thiago Gontijo <tjgontijo@gmail.com>
2025-07-20 13:34:39 -0300

    Update FloatingCta.tsx

--- src/app/desafio-literario/components/buttons/FloatingCta.tsx
+++ src/app/desafio-literario/components/buttons/FloatingCta.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { FaTag, FaClock, FaWhatsapp } from 'react-icons/fa';
+import { FaTag, FaClock } from 'react-icons/fa';
 
 interface FloatingCtaProps {
   paymentLink: string;
